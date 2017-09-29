ec916be9fef0c75906e130d4f488db2f2127bde9
ajaymatta <ajaykumar.mak@gmail.com>
2017-09-29 22:35:09 +0530

    Add files via upload

---
+++ models/interaction.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var InteractionSchema = new Schema({
+  id: {type: String},
+  user: {type:Number},
+  creationDate: {type: Date,default: Date.now},
+  resolvedQuery: {type: String},
+  action: {type: String},
+  contexts: [{
+        name: {type: String},
+        lifespan: {type: Number}
+  }],
+  response: {type: String}
+});
+
+module.exports = mongoose.model("Interaction", InteractionSchema);
